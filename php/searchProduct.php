67a2589d95359ac657b0f0e4e29a4c53f69d4261
minh997 <39399443+minh997@users.noreply.github.com>
2019-02-28 14:31:39 -0800

    Add files via upload

---
+++ php/searchProduct.php
@@ -0,0 +1,43 @@
+<?php
+
+$servername ="localhost";
+$username = "root"; //your username
+$password = "minhminh"; //your pasSword
+$database = "gamestore";
+
+//Getting values
+$PName = $_REQUEST["PName"];
+
+
+//Create connection
+$conn = new mysqli ($servername, $username, $password, $database);
+//Check connection
+if ($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+}else{
+    
+}
+
+$query = "SELECT product_stock.PID, product_details.PName, product_details.ReleaseDate,
+product_details.Price
+FROM product_stock
+INNER JOIN product_details
+ON product_stock.PName=product_details.PName
+WHERE product_stock.PName LIKE '%$PName%'";
+$result = mysqli_query($conn,$query);
+
+
+//store in array
+$data = array ();
+//while loop through database
+while ($row = mysqli_fetch_assoc($result)){
+    $data[] = $row;
+}
+
+//send $data to front end in JSON format
+echo json_encode($data);
+
+    
+    
+
+?>
