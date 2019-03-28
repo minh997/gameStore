489f0cc3d9b75e14c54c6ea9910ba4b8342cb3da
minh997 <39399443+minh997@users.noreply.github.com>
2019-03-28 05:34:18 -0700

    Fixed add function

--- php/AvgProdPrice.php
+++ php/AvgProdPrice.php
@@ -14,23 +14,23 @@ if ($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
 }
 
-$query ="SELECT Publisher, AVG(price) 'AvgPrice' from Product_Details group by publisher";
+$query ="SELECT Publisher, COUNT(PName) 'Products', ROUND(AVG(price)) 'Average Price By Publisher'
+from Product_Details
+group by publisher";
 
 $result = $conn->query($query);
 echo "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\">";
-echo "<link rel=\"stylesheet\" href=\"project.css\">
-<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\">
-<link rel=\"icon\" href=\"image/favicon.ico\" type=\"image/x-icon\">";
 if($result->num_rows > 0) {
-    echo " <table class=\"table table-hover\"><thead class=\"thead-dark\"><tr><th scope=\"col\">Publisher</th><th scope=\"col\">AvgPrice</th></tr></thead><tbody>";
+    echo " <table class=\"table table-hover\"><thead class=\"thead-dark\"><tr><th scope=\"col\">Publisher</th><th scope=\"col\">Products</th><th scope=\"col\">Average Price By Publisher</th></tr></thead><tbody>";
     while($row = $result->fetch_assoc()){
-        echo "<tr><th>".$row["Publisher"] . "</th><th>".$row["AvgPrice"]."</th></tr>";
+        echo "<tr><th>".$row["Publisher"] . "</th><th>".$row["Products"] . "</th><th>$".$row["Average Price By Publisher"]."</th></tr>";
     }
     echo "</tbody></table>";
 } else {
     echo "0 results";
 }
-echo "<button type=\"button\" onclick=\"window.location.href = '../productcontrol.html'\"> Back </button>";
+echo "<button style=\"position: relative; left: 50%;\" class=\"btn btn-success\" type=\"button\" onclick=\"window.location.href = '../productcontrol.html'\"> Back </button>";
+
 
 
 $conn->close();
